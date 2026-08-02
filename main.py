1def83219d367d55649a416f013d26873a59c7f1
Ramkrishno Mondal <0ramkrishno@gmail.com>
2026-08-02 11:21:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_photo(chat_id=update.effective_chat.id,photo=("https://i.postimg.cc/W3WTyJYM/IMG-1785647969787.jpg")
+    await context.bot.send_photo(chat_id=update.effective_chat.id,   photo=("https://i.postimg.cc/W3WTyJYM/IMG-1785647969787.jpg")
 
 if __name__ == '__main__':
     # আপনার এপিআই টোকেনটি এখানে বসান
